4fcb6b89bf8df6ed76d3b575f275f8ac86dda1bd
Asad Ahmed <asadrabbi@gmail.com>
2018-10-12 17:00:47 +0600

    Add files via upload

---
+++ Trenza Breadcrumbs/app/code/local/Trenza/Breadcrumbs/Model/Observer.php
@@ -0,0 +1,28 @@
+<?php
+
+class Trenza_Breadcrumbs_Model_Observer extends Mage_Core_Model_Observer
+{
+    public function addCategoryToBreadcrumbs(Varien_Event_Observer $observer)
+    {
+        #Mage::log('loaded', null, 'test.log', true);
+        
+        if (Mage::registry('current_category')) {
+            return;
+        }
+
+        $product = $observer->getProduct();
+
+        $product->setDoNotUseCategoryId(false);
+        $categoryIds = $product->getCategoryIds();
+
+        if (count($categoryIds)) {
+            $categories = Mage::getModel('catalog/category')->getCollection()
+                ->addAttributeToFilter('entity_id', $categoryIds)
+                ->addAttributeToFilter('is_active', 1);
+
+            $categories->getSelect()->order('level DESC')->limit(1);
+
+            Mage::register('current_category', $categories->getFirstItem());
+        }
+    }
+}
